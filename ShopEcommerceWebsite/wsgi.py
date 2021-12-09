f29f0dd854fa51b83dddf44e11135e4fd77f9ace
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-09 23:17:47 +0600

    First Commit

---
+++ ShopEcommerceWebsite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ShopEcommerceWebsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShopEcommerceWebsite.settings')
+
+application = get_wsgi_application()
